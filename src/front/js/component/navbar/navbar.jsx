4e96b17a6b896ccd27971146fcbe69462e505002
Raquel <raquelmartinc@gmail.com>
2022-10-19 10:55:25 +0000

    navbar solucion errores y encaminando diseño responsive

--- src/front/js/component/navbar/navbar.jsx
+++ src/front/js/component/navbar/navbar.jsx
@@ -21,69 +21,45 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-color p-0">
+    <nav className="navbar navbar-expand-lg navbar-color">
       <div className="container-fluid">
-        <div className="navbar-brand mt-2 mt-lg-0">
+        <div className="navbar-brand">
           <img src={logo} alt="GOutside Logo" style={{ width: "4rem" }} />
         </div>
-        <button
-          type="button"
-          className="navbar-toggler btn-collapse"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="navbar-toggler-icon">
-            <BsList />
-          </span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="inputbox">
-            <input
-              placeholder="Buscar..."
-              type="text"
-              className="navbar-input"
-              required=""
-            />
+        <div className="col-6 col-md-6 col-lg-9">
+          <div className="d-flex">
+            <form className="">
+              <input
+                placeholder="Buscar..."
+                type="text"
+                className="navbar-input"
+              />
+            </form>
+            <div className="d-flex justify-content-around">
+              <Link to="/home/user">
+                <button className="btn btn-menu mx-2">
+                  <BsHouseFill className="btn-icon" />
+                  <span className="btn-text">HOME</span>
+                </button>
+              </Link>
+              <Link to="/video-library">
+                <button className="btn btn-menu mx-2">
+                  <BsFillCollectionPlayFill className="btn-icon" />
+
+                  <span className="btn-text">BIBLIOTECA</span>
+                </button>
+              </Link>
+              <Link to="/all-commpetition">
+                <button className="btn btn-menu mx-2">
+                  <BsFillTrophyFill className="btn-icon" />
+
+                  <span className="btn-text">COMPETICIONES</span>
+                </button>
+              </Link>
+            </div>
           </div>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <Link to="/home/user">
-              <li className="nav-item">
-                <button className="btn btn-menu mx-2">
-                  <span className="btn-icon" style={{ fontSize: "1rem" }}>
-                    <BsHouseFill />
-                  </span>
-                  <span className="btn-text" style={{ fontSize: "1rem" }}>
-                    HOME
-                  </span>
-                </button>
-              </li>
-            </Link>
-            <Link to="/video-library">
-              <li className="nav-item">
-                <button className="btn btn-menu mx-2">
-                  <span className="btn-icon" style={{ fontSize: "1rem" }}>
-                    <BsFillCollectionPlayFill />
-                  </span>
-                  <span className="btn-text" style={{ fontSize: "1rem" }}>
-                    BIBLIOTECA
-                  </span>
-                </button>
-              </li>
-            </Link>
-            <Link to="/all-commpetition">
-              <li className="nav-item">
-                <button className="btn btn-menu mx-2">
-                  <span className="btn-icon" style={{ fontSize: "1rem" }}>
-                    <BsFillTrophyFill />
-                  </span>
-                  <span className="btn-text" style={{ fontSize: "1rem" }}>
-                    COMPETICIONES
-                  </span>
-                </button>
-              </li>
-            </Link>
-          </ul>
         </div>
+
         <div className="btn-group">
           <button
             className="btn dropdown-toggle"

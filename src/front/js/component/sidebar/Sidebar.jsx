ac88e6d6b63ea42ac95cdb7b8777bfb30462ea3d
Raquel <raquelmartinc@gmail.com>
2022-10-19 08:04:22 +0000

    first commit

--- src/front/js/component/sidebar/Sidebar.jsx
+++ src/front/js/component/sidebar/Sidebar.jsx
@@ -6,28 +6,52 @@ function Sidebar() {
   return (
     <div className="container-fluid">
       <div className="row flex-nowrap">
-        {/* <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-          <div class="dropdown pb-4">
-                    <a href="#" class="d-flex align-items-center " data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src={GOutside_transp} alt="hugenerd" class="rounded-circle" />
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                        <li><a class="dropdown-item" href="#">Editar perfil</a></li>
-                        <li><a class="dropdown-item" href="#">Sobre nosotros</a></li>
-                        <li>
-                            <hr class="dropdown-divider" />
-                        </li>
-                        <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
-                    </ul>
-                </div>
+            <div class="dropdown pb-4">
+              <a
+                href="#"
+                class="d-flex align-items-center "
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src={GOutside_transp}
+                  alt="hugenerd"
+                  class="rounded-circle"
+                />
+              </a>
+              <ul
+                class="dropdown-menu dropdown-menu-dark text-small shadow"
+                aria-labelledby="dropdownUser1"
+              >
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Editar perfil
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Sobre nosotros
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Cerrar sesión
+                  </a>
+                </li>
+              </ul>
+            </div>
             <ul
               className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
               id="menu"
             >
               <li className="nav-item">
                 <a href="#" className="nav-link align-middle px-0">
-                <i class="fs-4 bi bi-person"></i>{" "}
+                  <i class="fs-4 bi bi-person"></i>{" "}
                   <span className="ms-1 d-none d-sm-inline">Mi perfil</span>
                 </a>
               </li>
@@ -47,21 +71,25 @@ function Sidebar() {
                 >
                   <li className="w-100">
                     <a href="#" className="nav-link px-0">
-                    <i className="fs-6 bi-pencil-square"></i>{" "}
-                    <span className="ms-1 d-none d-sm-inline">Crear competición</span>{" "}
+                      <i className="fs-6 bi-pencil-square"></i>{" "}
+                      <span className="ms-1 d-none d-sm-inline">
+                        Crear competición
+                      </span>{" "}
                     </a>
                   </li>
                   <li>
                     <a href="#" className="nav-link px-0">
                       <i className="fs-6 bi-stopwatch"></i>{" "}
-                      <span className="ms-1 d-none d-sm-inline">Mis competiciones</span>{" "}
+                      <span className="ms-1 d-none d-sm-inline">
+                        Mis competiciones
+                      </span>{" "}
                     </a>
                   </li>
                 </ul>
               </li>
             </ul>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );

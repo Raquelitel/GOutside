971ae747d597d374aee27883c50e026e131b91e8
Raquel <raquelmartinc@gmail.com>
2022-11-07 18:44:44 +0000

    deploying to heroku

--- migrations/versions/5a53141b6818_.py
+++ migrations/versions/5a53141b6818_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: e74aab5d8be8
+Revision ID: 5a53141b6818
 Revises: 
-Create Date: 2022-11-07 09:56:19.360280
+Create Date: 2022-11-07 18:44:26.340978
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = 'e74aab5d8be8'
+revision = '5a53141b6818'
 down_revision = None
 branch_labels = None
 depends_on = None
